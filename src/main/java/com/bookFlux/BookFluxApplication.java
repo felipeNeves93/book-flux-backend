2cab839c355d809a4243e4169fa9e492aa44016e
felipe-neves <felipe.neves@connectist.com>
2025-04-14 21:08:26 -0300

    - Initial commit of the repository

---
+++ src/main/java/com/bookFlux/BookFluxApplication.java
@@ -0,0 +1,13 @@
+package com.bookFlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookFlux/BookFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookFlux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
