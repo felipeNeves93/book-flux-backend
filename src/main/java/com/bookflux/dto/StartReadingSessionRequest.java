d2acc2d9c5547483624ea4cd44b50b7b3f2d56ab
felipe-neves <felipe.neves@connectist.com>
2025-06-11 22:52:25 -0300

    - Fixes merge

--- src/main/java/com/bookflux/dto/StartReadingSessionRequest.java
+++ src/main/java/com/bookflux/dto/StartReadingSessionRequest.java
@@ -1,33 +1,33 @@
-    package com.bookflux.dto;
+package com.bookflux.dto;
 
-    import com.bookflux.repository.collection.UserBookCollection;
-    import jakarta.validation.Valid;
-    import jakarta.validation.constraints.Future;
-    import jakarta.validation.constraints.NotBlank;
-    import jakarta.validation.constraints.NotNull;
-    import jakarta.validation.constraints.PastOrPresent;
-    import lombok.AllArgsConstructor;
-    import lombok.Getter;
-    import lombok.NoArgsConstructor;
-    import lombok.Setter;
+import com.bookflux.repository.collection.book.UserBookCollection;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-    import java.time.LocalDateTime;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StartReadingSessionRequest {
 
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public class StartReadingSessionRequest {
-        @NotBlank
-        private String bookId;
-        @NotBlank
-        private String userId;
-        @PastOrPresent
-        private LocalDateTime startTime;
-        @Future
-        private LocalDateTime endTime;
+  @NotBlank
+  private String bookId;
+  @NotBlank
+  private String userId;
+  @PastOrPresent
+  private LocalDateTime startTime;
+  @Future
+  private LocalDateTime endTime;
 
-        @NotNull
-        @Valid
-        private UserBookCollection userBookCollection;
-    }
+  @NotNull
+  @Valid
+  private UserBookCollection userBookCollection;
+}
